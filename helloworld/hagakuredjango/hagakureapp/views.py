97bc0a08f01e4bb584d319789cc7a7f2ede2f905
furukawa3152 <furukawa3152@gmail.com>
2024-02-25 23:28:20 +0900

    トランスフォーマーサガエラー処理

--- helloworld/hagakuredjango/hagakureapp/views.py
+++ helloworld/hagakuredjango/hagakureapp/views.py
@@ -18,17 +18,17 @@ def saga_bot(request):
 # API
 @require_http_methods(["POST"])
 def trans_sagaben(request):
-    user_message = request.POST.get('message')
+    # 改行を取り除いて入力を処理
+    user_message = request.POST.get('message', '').replace('\n', '').replace('\r', '')
     try:
-        response = requests.post("https://9z6fdi7vqg.execute-api.ap-northeast-1.amazonaws.com/default/Transformer_saga", # グローバルIPとポート番号を修正
+        response = requests.post("https://9z6fdi7vqg.execute-api.ap-northeast-1.amazonaws.com/default/Transformer_saga",
                                  json={"message": user_message})
         response.raise_for_status()
     except requests.exceptions.RequestException as err:
-        print(f"An error occurred: {err}")
-        return render(request, "saga_bot.html", {"messages": [{"sender": "bot", "text": "An error occurred while processing your request. Please try again later."}]})
+        print(f"エラーばい。入力内容ば確認して試してくれん？: {err}")
+        return render(request, "saga_bot.html", {"messages": [{"sender": "bot", "text": "エラーばい。入力内容ば確認して試してくれん？"}]})
     else:
         return render(request, "saga_bot.html", {"messages": response.json()["messages"]})
-    
 # T5説明ページ
 def indext5explain(request):
     return render(request, 'indext5explain.html')
